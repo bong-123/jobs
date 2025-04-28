01342527d03d08f0a34fd7b148cbde772a6109ff
bong-123 <git config --global user.email filipinascarlchristian@gmail.com>
2025-04-28 13:00:29 +0800

    initial commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
 'use client';
 import Link from 'next/link';
+
 const Header: React.FC = () => {
   return (
-    <header className="w-full bg-white shadow p-4 flex justify-between items-center fixed top-0">
-      <h2 className="text-xl font-bold text-gray-800">Job Application Tracker</h2>
-      <nav className="flex gap-4">
+    <header className="w-full bg-white shadow p-4 flex flex-col md:flex-row justify-between items-center fixed top-0 z-50">
+      <h2 className="text-xl font-bold text-gray-800 mb-2 md:mb-0">
+        Job Application Tracker
+      </h2>
+      <nav className="flex flex-col md:flex-row gap-2 md:gap-4 items-center">
         <Link href="/" className="text-gray-600 font-semibold hover:text-red-800">
           Home
         </Link>
